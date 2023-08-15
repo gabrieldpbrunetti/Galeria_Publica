1cd1efabb24fb608699524a7ba150ead17a002ae
gabrieldpbrunetti <gabrieldpbrunetti@gmail.com>
2023-08-15 11:02:59 -0300

    15/08

--- app/src/main/java/brunetti/depaula/galeriapublica/MainActivity.java
+++ app/src/main/java/brunetti/depaula/galeriapublica/MainActivity.java
@@ -1,13 +1,19 @@
 package brunetti.depaula.galeriapublica;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
@@ -18,7 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    static int RESULT_REQUEST_PERMISSION = 2;
     BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,5 +65,74 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         List<String> permissions = new ArrayList<>();
         permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        checkForPermissions(permissions);
+    }
+
+    private void checkForPermissions(List<String> permissions){
+        List<String> permissionsNotGranted = new ArrayList<>();
+
+        //percorre o array de strings, se houver alguma permissao nao concedida, adiciona a mesma no array permissionsNotGranted
+        for(String permission : permissions){
+            if(!hasPermission(permission)){
+                permissionsNotGranted.add(permission);
+            }
+        }
+
+        //checa se a versao de desenvolvedor e maior ou igual a versao marshmallow
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            //se houve permissoes nao concedidas, faz a requisicao da permissao
+            if(permissionsNotGranted.size() > 0){
+                requestPermissions(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]), RESULT_REQUEST_PERMISSION);
+            }
+            else {
+                MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+                int navigationOpSelected = vm.getNavigationOpSelected();
+                bottomNavigationView.setSelectedItemId(navigationOpSelected);
+            }
+        }
+    }
+
+    //checa se o aplicativo possui uma permissao
+    private boolean hasPermission(String permission){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            return ActivityCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+        final List<String> permissionRejected = new ArrayList<>();
+        //checa se o aplicativo possui uma permissao, se nao houver adiciona no array permissionRejected
+        if(requestCode == RESULT_REQUEST_PERMISSION){
+            for(String permission : permissions){
+                if(!hasPermission(permission)){
+                    permissionRejected.add(permission);
+                }
+            }
+        }
+
+        //se houver permissoes nao concediadas, mostra um alerta na tela que avisa o usuario que a permissao pedida e necessaria para o aplicativo funcionar
+        //tambem mostra um botao que ao ser clicado faz a requisicao das permissoes de novo
+        if(permissionRejected.size() > 0){
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if(shouldShowRequestPermissionRationale(permissionRejected.get(0))){
+                    new AlertDialog.Builder(MainActivity.this).setMessage("Para usar essa app é necessário conceder essas permissões").setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int which) {
+                            requestPermissions(permissionRejected.toArray(new String[permissionRejected.size()]), RESULT_REQUEST_PERMISSION);
+                        }
+                    }).create().show();
+                }
+            }
+        }
+        else{
+            MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+            int navigationOpSelected = vm.getNavigationOpSelected();
+            bottomNavigationView.setSelectedItemId(navigationOpSelected);
+        }
     }
 }
