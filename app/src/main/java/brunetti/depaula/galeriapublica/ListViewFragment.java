77782cfe189c19d7870e1ebe31e0e7f1d3ccb47d
gabrieldpbrunetti <gabrieldpbrunetti@gmail.com>
2023-09-05 10:35:17 -0300

    05/09

--- app/src/main/java/brunetti/depaula/galeriapublica/ListViewFragment.java
+++ app/src/main/java/brunetti/depaula/galeriapublica/ListViewFragment.java
@@ -40,14 +40,7 @@ public class ListViewFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListViewFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static ListViewFragment newInstance() {
         ListViewFragment fragment = new ListViewFragment();
